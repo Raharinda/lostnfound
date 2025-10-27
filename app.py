e968a9191b0edab4378744bdbaf1f715e2cc73bc
Raharinda <raharindanadhif@gmail.com>
2025-10-27 13:13:48 +0700

    routing

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/report')
+def report():
+    return render_template('/report/reportFound.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

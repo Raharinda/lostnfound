8177b7321668d29b363f7ea77a1a5ea822e84b7e
Raharinda <raharindanadhif@gmail.com>
2025-11-17 12:23:34 +0700

    fix auth internal server error

--- app/__init__.py
+++ app/__init__.py
@@ -58,12 +58,12 @@ def create_app():
         name="google",
         client_id=os.getenv("GOOGLE_CLIENT_ID"),
         client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
-        access_token_url='https://oauth2.googleapis.com/token',
-        authorize_url='https://accounts.google.com/o/oauth2/auth',
-        api_base_url='https://www.googleapis.com/oauth2/v2/',
-        userinfo_endpoint='https://www.googleapis.com/oauth2/v2/userinfo',
-        client_kwargs={'scope': 'openid email profile'}
-    )
+        server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
+        client_kwargs={
+            "scope": "openid email profile"
+        }
+)
+
 
     # ====================================
     # BLUEPRINTS
